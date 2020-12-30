3da61ff0d2cd7a1b3d9291ec3f6152590b59e07d
Kingston802 <alexkingstonbailey@gmail.com>
2020-12-30 22:28:13 +1300

    changed to sprites

--- main.cpp
+++ main.cpp
@@ -6,81 +6,127 @@ class BreakOut : public olc::PixelGameEngine
 public:
 	BreakOut()
 	{
-		sAppName = "TUTORIAL - BreakOut Clone";
+		sAppName = "BreakOut Clone";
 	}
 
 private: 
   float fBatPos = 20.0f;
   float fBatWidth = 40.0f;
-
   olc::vf2d vBall = { 200.0f, 200.0f };
   olc::vf2d vBallVel = { 200.0f, -100.0f };
+  olc::vf2d vBallDir = { 0.0f, 0.0f };
+  olc::vf2d vBallPos = { 0.0f, 0.0f };
   float fBatSpeed = 250.0f;
+  float fBallSpeed = 20.0f;
   float fBallRadius = 5.0f;
+  olc::vi2d vBlockSize = { 16,16 };
+  std::unique_ptr<int[]> blocks;
+
+  std::unique_ptr<olc::Sprite> sprTile;
 
 public:
 	bool OnUserCreate() override
 	{
+    blocks = std::make_unique<int[]>(24*30);
+    for (int y = 0; y < 30; y++) 
+    { 
+      for (int x = 0; x < 24; x++) 
+      { 
+        if (x == 0 || y == 0 || x == 23)
+          blocks[y * 24 + x] = 10;
+        else
+          blocks[y * 24 + x] = 0;
+
+        if (x > 2 && x <= 20 && y > 3 && y <= 5)
+          blocks[y * 24 + x] = 1;
+        if (x > 2 && x <= 20 && y > 5 && y <= 7)
+          blocks[y * 24 + x] = 2;
+        if (x > 2 && x <= 20 && y > 7 && y <= 9)
+          blocks[y * 24 + x] = 3;
+      }
+    }
+
+    sprTile = std::make_unique<olc::Sprite>("tut_tile.png");
+
+    // start ball
+    float fAngle = float(rand()) / float(RAND_MAX) * 2.0f * 3.14159f;
+    fAngle = -0.4f;
+    vBallDir = { cos(fAngle), sin(fAngle) };
+    vBallPos = { 12.5f, 15.5f };
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-    // Handle user input 
-    if (GetKey(olc::Key::LEFT).bHeld) fBatPos -= fBatSpeed * fElapsedTime;
-    if (GetKey(olc::Key::RIGHT).bHeld) fBatPos += fBatSpeed * fElapsedTime;
+    // Calculate where ball should be, if no collision
+    olc::vf2d vPotentialBallPos = vBallPos + vBallDir * fBallSpeed * fElapsedTime;
+    // Test for hits 4 points around ball
+    olc::vf2d vTileBallRadialDims = { fBallRadius / vBlockSize.x, fBallRadius / vBlockSize.y };
 
-    if (fBatPos < 11.0f) fBatPos = 11.0f;
-    if (fBatPos + fBatWidth > float(ScreenWidth()) - 10.0f) fBatPos = float(ScreenWidth()) - 10.0f - fBatWidth;
+    auto TestResolveCollisionPoint = [&](const olc::vf2d& point)
+		{
+			olc::vi2d vTestPoint = vPotentialBallPos + vTileBallRadialDims * point;
 
-    // Update ball
-    vBall += vBallVel * fElapsedTime;
+			auto& tile = blocks[vTestPoint.y * 24 + vTestPoint.x];
+			if (tile == 0)
+			{
+				// Do Nothing, no collision
+				return false;
+			}
+			else
+			{
+				// Ball has collided with a tile
+				bool bTileHit = tile < 10;
+				if (bTileHit) tile--;
+				
+				// Collision response
+				if (point.x == 0.0f) vBallDir.y *= -1.0f;				
+				if (point.y == 0.0f) vBallDir.x *= -1.0f;
+				return bTileHit;
+			}
+		};
 
-    // Really crude arena detection 
-    // flip in y direction
-    if (vBall.y <= 10.0f) vBallVel.y *= -1.0f;
-    // flip in x direction
-    if (vBall.x <= 10.0f) vBallVel.x *= -1.0f;
-    // flip in x direction (other side)
-    if (vBall.x >= float(ScreenWidth()) - 10.0f) vBallVel.x *= -1.0f;
+    bool bHasHitTile = false;
+    bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(0, -1));
+		bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(0, +1));
+		bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(-1, 0));
+		bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(+1, 0));
 
-    // Check Bat 
-    if (vBall.y >= (float(ScreenHeight()) - 20.0f) && (vBall.x > fBatPos) && (vBall.x < fBatPos + fBatWidth))
-      vBallVel.y *= -1.0f;
+    // Fake floor
+    if (vBallPos.y > 20.0f) vBallDir.y *= -1.0f;
 
-    // check if ball has gone off screen
-    if (vBall.y > ScreenHeight()) 
-    {
-      // reset ball location
-      vBall = { 200.0f, 200.0f };
-      // choose random direction 
-      float fAngle = (float(rand()) / float(RAND_MAX)) * 2.0f * 3.14159f;
-      vBallVel = { 300.0f * float(cos(fAngle)), 300.0f * float(sin(fAngle)) };
-    }
+    // update ball position 
+    vBallPos += vBallDir * fBallSpeed * fElapsedTime;
 
-    if (GetMouse(0).bHeld)
-    { 
-      vBall = { float(GetMouseX()), float(GetMouseY()) };
-    }
-
-    if (GetMouseWheel() > 0) fBallRadius += 1.0f;
-		if (GetMouseWheel() < 0) fBallRadius -= 1.0f;
-		if (fBallRadius < 5.0f) fBallRadius = 5.0f;
-
-    // Erase previous frame 
+    // Draw Screen
     Clear(olc::DARK_BLUE);
+    SetPixelMode(olc::Pixel::MASK); // no pixels that have transparency 
+    for (int y = 0; y < 30; y++)
+    { 
+      for (int x = 0; x < 24; x++) 
+      { 
+        switch (blocks[y * 24 + x])
+        { 
+          case 0: // Do nothing 
+            break;
+          case 10: // Draw Boundary
+            /* FillRect(olc::vi2d(x, y) * vBlockSize, vBlockSize, olc::WHITE); */
+            DrawPartialSprite(olc::vi2d(x, y) * vBlockSize, sprTile.get(), olc::vi2d(0, 0) * vBlockSize, vBlockSize);
+            break;
+          case 1: // Draw Red Block 
+            DrawPartialSprite(olc::vi2d(x, y) * vBlockSize, sprTile.get(), olc::vi2d(1, 0) * vBlockSize, vBlockSize);
+            break;
+          case 2: // Draw Green Block 
+            DrawPartialSprite(olc::vi2d(x, y) * vBlockSize, sprTile.get(), olc::vi2d(2, 0) * vBlockSize, vBlockSize);
+            break;
+          case 3: // Draw Yellow Block 
+            DrawPartialSprite(olc::vi2d(x, y) * vBlockSize, sprTile.get(), olc::vi2d(3, 0) * vBlockSize, vBlockSize);
+            break;
+        }
+      }
+    }
 
-    // Draw Boundary 
-    DrawLine(10, 10, 502, 10, olc::YELLOW);
-    DrawLine(10, 10, 10, 470, olc::YELLOW);
-    DrawLine(502, 10, 502, 470, olc::YELLOW);
-
-    // Draw Bat
-    FillRect(int(fBatPos), ScreenHeight() - 20, int(fBatWidth), 10, olc::GREEN);
-
-    // Draw Ball 
-    FillCircle(vBall, 5, olc::CYAN);
-
+    FillCircle(vBallPos * vBlockSize, fBallRadius, olc::CYAN);
     return true;
 	}
 };
@@ -88,7 +134,7 @@ public:
 int main()
 {
 	BreakOut demo;
-	if (demo.Construct(512, 480, 2, 2))
+	if (demo.Construct(512, 480, 1, 1))
 		demo.Start();
 	return 0;
 }
